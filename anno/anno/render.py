d4d0e4e476d3e1911b377a4c58b8fe3cfab7c05f
Gregory Gundersen <ggundersen@gmail.com>
2020-06-29 10:00:28 -0400

    Color links.

--- anno/anno/render.py
+++ anno/anno/render.py
@@ -18,7 +18,8 @@ def render_markdown(value):
 
 
 def make_pdf(note):
-    extra_args = ['-V', 'geometry:margin=1in', '--pdf-engine', 'pdflatex']
+    extra_args = ['-V', 'geometry:margin=1in', '--pdf-engine', 'pdflatex',
+				  '-V', 'colorlinks', '-V', 'urlcolor=NavyBlue']
 
     # FIXME. This is hacky. I'm replacing
     #   [my caption](/image/foo.png)
