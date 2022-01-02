f3e40b6be9d38f8b934b9b45209fc70c3d1fe7d4
Gregory Gundersen <ggundersen@gmail.com>
2022-01-01 21:57:51 -0500

    New version #.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ description = 'Anno is a thin user interface on Markdown files for easy and ' \
 
 setup_args = dict(
     name='anno',
-    version='0.0.3',
+    version='0.0.4',
     license='MIT',
     author='Gregory Gundersen',
     author_email='greg@gregorygundersen.com',
